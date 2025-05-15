60757444196baba4fd55de6b69578c5eb51fa743
addhyan nigam <addhyannigam@gmail.com>
2025-05-15 22:38:30 +0530

    added requiremnts.txt and lib_install.sh file

---
+++ lib_install.sh
@@ -0,0 +1,5 @@
+pip install -r requirements.txt
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
+python -m spacy download en_core_web_sm
+pip install yt-dlp
+pip install git+https://github.com/mps-youtube/pafy.git
